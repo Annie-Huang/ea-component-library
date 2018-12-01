df48be73bb30a2eca36acd6c102ad79df6ebf614
Annie Huang <annie.hn@gmail.com>
2018-12-01 19:00:47 +1100

    Remove unnecessary service for demo page.

--- src/app/showcase/app.module.ts
+++ src/app/showcase/app.module.ts
@@ -10,10 +10,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AutoCompleteModule } from '../components/autocomplete/autocomplete';
 
-import { CarService } from './service/carservice';
-import { CountryService } from './service/countryservice';
-import { EventService } from './service/eventservice';
-import { NodeService } from './service/nodeservice';
 import { IconService } from './service/iconservice';
 
 @NgModule({
@@ -32,7 +28,7 @@ import { IconService } from './service/iconservice';
   ],
   providers: [
       { provide: LocationStrategy, useClass: HashLocationStrategy },
-      CarService,CountryService,EventService,NodeService,IconService
+      IconService
   ],
   bootstrap: [AppComponent]
 })
