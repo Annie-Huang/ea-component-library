8121715e84dbea51231baa63a58a715fd5eccaa1
Annie Huang <annie.hn@gmail.com>
2018-12-03 15:22:34 +1100

    Change the entry file of the npm package from primeng.js, primeng.d.ts to eauicomp.js, eauicomp.d.ts

---
+++ eauicomp.js
@@ -0,0 +1,14 @@
+/* Shorthand */
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./components/common/api"));
+__export(require("./components/common/shared"));
+__export(require("./components/autocomplete/autocomplete"));
+__export(require("./components/button/button"));
+__export(require("./components/codehighlighter/codehighlighter"));
+__export(require("./components/inputtext/inputtext"));
+__export(require("./components/tabview/tabview"));
+__export(require("./components/tooltip/tooltip"));
