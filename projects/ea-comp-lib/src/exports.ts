8977c981662dbf28a019d65e043ad2a1effa3ea7
Annie Huang <annie.hn@gmail.com>
2018-12-13 15:01:58 +1100

    Branch05: Removed the ea-comp-lib service, cmoponent and module. Test package in another app. Success.

--- projects/ea-comp-lib/src/exports.ts
+++ projects/ea-comp-lib/src/exports.ts
@@ -2,8 +2,5 @@
  * Public API Surface of ea-comp-lib
  */
 
-export * from './lib/ea-comp-lib.service';
-export * from './lib/ea-comp-lib.component';
-export * from './lib/ea-comp-lib.module';
 export * from './lib/foo/foo.module';
 export * from './lib/button2/button';
